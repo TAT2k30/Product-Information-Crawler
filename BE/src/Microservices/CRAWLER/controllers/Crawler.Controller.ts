2e0b926a6ec8c830f7857e71efd3672189ce8e67
anh tien <anhtienb237@gmail.com>
2024-11-07 14:50:30 +0700

    Update logic for ebay crawler, fix bugs

--- BE/src/Microservices/CRAWLER/controllers/Crawler.Controller.ts
+++ BE/src/Microservices/CRAWLER/controllers/Crawler.Controller.ts
@@ -3,50 +3,108 @@ import * as cheerio from "cheerio";
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
+const extractNumericPrice = (priceText: string): number | null => {
+  const numericString = priceText.replace(/[^0-9.]/g, "");
+  const price = parseFloat(numericString);
+  return isNaN(price) ? null : price;
+};
+
+const fetchAmazonProductData = ($: cheerio.Root, url: string) => {
+  const productName = $("#productTitle").text().trim();
+  const currentPriceText = $(".a-price .a-offscreen").text().trim();
+  const currentPrice = parseFloat(currentPriceText.replace(/[$,]/g, ""));
+  const imageUrl = $("#landingImage").attr("src");
+  const bulletPoints: string[] = [];
+  $("#feature-bullets ul.a-unordered-list li.a-spacing-mini span.a-list-item").each((_, element) => {
+    bulletPoints.push($(element).text().trim());
+  });
+  console.log("Bullet points " + bulletPoints);
+  const description = bulletPoints.join("\n");
+  const platformLinks = [{ platform: "Amazon", url }];
+
+  return productName && currentPrice && imageUrl
+    ? {
+        productName,
+        description,
+        imageUrl,
+        currentPrice,
+        platformLinks,
+        ratings: {
+          averageRating: 0,
+          reviewCount: 0,
+        },
+        fetchDate: new Date(),
+        source: "Amazon",
+      }
+    : null;
+};
+
+const fetchEbayProductData = ($: cheerio.Root, url: string) => {
+  const productName = $("div.vim")
+    .find("h1.x-item-title__mainTitle")
+    .text()
+    .trim();
+  // console.log("Product name: " + productName);
+  const currentPriceText = $(".x-price-primary .ux-textspans").text().trim();
+  const currentPrice = extractNumericPrice(currentPriceText);
+  // console.log("Current price : " + currentPrice);
+
+  const imageUrl = $("div.ux-image-carousel-item").find("img").attr("src");
+  // console.log("Image url: " + imageUrl);
+
+  const platformLinks = [{ platform: "Ebay", url }];
+
+  return productName && currentPrice && imageUrl
+    ? {
+        productName,
+        imageUrl,
+        currentPrice,
+        platformLinks,
+        ratings: {
+          averageRating: 0,
+          reviewCount: 0,
+        },
+        fetchDate: new Date(),
+        source: "Ebay",
+      }
+    : null;
+};
+
 export const handleFetchProductData = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   const { url } = req.body;
+  const isAmazon = url.includes("amazon");
+  const isEbay = url.includes("ebay");
+
+  if (!isAmazon && !isEbay) {
+    res.status(501).json({
+      success: false,
+      message: "Not supported platform",
+    });
+  }
 
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
-    // Lọc thông tin sản phẩm
-    const productName = $("#productTitle").text().trim();
-    const currentPriceText = $(".a-price .a-offscreen").text().trim();
-    const currentPrice = parseFloat(currentPriceText.replace(/[$,]/g, ""));
-    const imageUrl = $("#landingImage").attr("src");
-    const description = $(".a-unordered-list").text().trim();
-    const platformLinks = [{ platform: "Amazon", url }];
+    const productData = isAmazon
+      ? fetchAmazonProductData($, url)
+      : isEbay
+      ? fetchEbayProductData($, url)
+      : null;
 
-    if (!productName || !currentPrice || !imageUrl) {
-      res
-        .status(404)
-        .json({ success: false, message: "Product information not found" });
+    if (!productData) {
+      res.status(404).json({
+        success: false,
+        message: "Product information not found",
+      });
     }
 
-    // Tạo đối tượng sản phẩm
-    const productData = {
-      productName,
-      description,
-      imageUrl,
-      currentPrice,
-      platformLinks,
-      ratings: {
-        averageRating: 0,
-        reviewCount: 0,
-      },
-      fetchDate: new Date(),
-      source: "Amazon",
-    };
-
-    // Lưu sản phẩm vào cơ sở dữ liệu
     const newProduct = new Product(productData);
     await newProduct.save();
 
-    // Trả về phản hồi
     res.status(200).json({
       success: true,
       data: productData,
